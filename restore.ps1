0750db9d46c10a229aeed8fad17baf69e1444e4a
sparky256DSL <sparks.shaun@mchsi.com>
2014-04-12 10:41:57 -0500

    Work in progress
    This is what I have done so far. Still a long ways to go. Problem correlating the drive letter to the vmdk at this point, then get path of vmdk to start issuing commands to the Netapp.

---
+++ restore.ps1
@@ -0,0 +1,26 @@
+Connect-VIServer vcenter -User "lab\administrator" -Password "$unshine256"
+cls
+Do {
+$vmname = read-host -prompt "Enter the name of the VM to restore files from"
+} while ($vmname -eq $null)
+
+$logicaldrives = Get-WmiObject win32_logicaldisk -ComputerName $vmname -Credential administrator | Where-Object {$_.DeviceType -eq '3'} | foreach { $_.DeviceID }
+
+$selectedDrive = $false
+Do {
+    Write-Host ""
+    foreach($d in $logicaldrives)
+    {
+        
+            Write-Host $d
+        
+    }
+    $choice1 = read-host -prompt "Select a drive letter (example C:)"
+    foreach($d in $logicaldrives)
+    {
+        if($d -eq $choice1)
+           {
+                $selectedDrive = $true
+           }
+    }
+} until ($selectedDrive -eq $true )
