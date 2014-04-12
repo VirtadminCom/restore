95a595815fd2381c4faafc1a112c8e547e8d7b1f
James <james@virtadmin.com>
2014-04-12 13:08:29 -0500

    Now returning the correct VMDK that a user will choose from a list in the variable .

--- main.ps1
+++ main.ps1
@@ -8,13 +8,35 @@ Add-PsSnapin VMware.VimAutomation.Core
 Import-Module .\Get-VMDiskMap.ps1
 
 #Collect credentials
-$VCserver = Read-Host -prompt "Enter FQDN of vCenter to connect to" 
-Write-Host -ForegroundColor Green "You will now be prompted to enter your credentials for connecting to vCenter"
+Write-Host -ForegroundColor Green "Enter FQDN of vCenter to connect to: " -NoNewLine 
+$VCserver = Read-Host 
+Write-Host -ForegroundColor Yellow "You will now be prompted to enter your credentials for connecting to vCenter"
 Sleep 3
 $VCcreds = Get-Credential $null
-Write-Host -ForegroundColor Green "You will now be prompted to enter your credentials to connect to domain computers"
+Write-Host -ForegroundColor Yellow "You will now be prompted to enter your credentials to connect to domain computers"
 Sleep 3
 $ADcreds = Get-Credential $null
 
 #Connect to vCenter Server
-Connect-VIServer $VCserver -Credential $VCcreds > $NUL 
+Connect-VIServer $VCserver -Credential $VCcreds > $NUL
+
+#Get the VMDK file to operate on
+Write-Host -ForegroundColor Green "Enter the name of the VM to restore a disk from: "  -NoNewLine 
+$SrcVM = Read-Host 
+$disks = Get-VMDiskMap $SrcVM $ADcreds | Select DiskFile,DiskSize,WindowsDisks
+[int]$VMDKchoice = 0
+while ( $VMDKchoice -lt 1 -or $VMDKchoice -gt $disks.Count){
+    $diskno = 0
+    Foreach ($disk in $disks){
+        $diskno = $diskno + 1
+        Write-Host $diskno. $disk.WindowsDisks
+    }
+Write-Host -ForegroundColor Green "Please enter the menu number of the disk to restore: " -NoNewLine 
+[Int]$VMDKchoice = Read-Host }
+$SrcDisk = $disks[$VMDKchoice-1].DiskFile
+$SrcDisk
+# Switch( $VMDKchoice ){
+  # 1{#run an action or call a function here }
+  # 2{<run an action or call a function here #>}
+  # 3{<#run an action or call a function here #>}
+# default{<#run a default action or call a function here #>}
