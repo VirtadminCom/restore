16404c6bc3a02ca82bad252f19fcc9a2cc996bce
James <james@virtadmin.com>
2014-04-12 12:03:22 -0500

    Modified Get-VMDiskMap module to accept AD credentials as a positional parameter. Requested creds at the beginning of restore script.

--- Get-VMDiskMap.ps1
+++ Get-VMDiskMap.ps1
@@ -1,8 +1,7 @@
 Function Get-VMDiskMap {
     [Cmdletbinding()]
-    param([Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)][string]$VM)
-    begin {
-    }
+    param([Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$true)][string]$VM,
+        [Parameter(Position=1,Mandatory=$true,ValueFromPipeline=$true)][ValidateNotNull()][System.Management.Automation.PSCredential][System.Management.Automation.Credential()]$ADcreds)
     process {
         if ($VM) {
             $VmView = Get-View -ViewType VirtualMachine -Filter @{"Name" = $VM}    
@@ -15,15 +14,14 @@ Function Get-VMDiskMap {
                     $VirtualDisk.SCSI_Id = "$($VirtualSCSIController.BusNumber) : $($VirtualDiskDevice.UnitNumber)"
                     $VirtualDisk.DiskFile = $VirtualDiskDevice.Backing.FileName
                     $VirtualDisk.DiskSize = $VirtualDiskDevice.CapacityInKB * 1KB / 1GB
-
                     $LogicalDisks = @()
                     # Look up path for this disk using WMI.
-                    $thisVirtualDisk = get-wmiobject -class "Win32_DiskDrive" -namespace "root\CIMV2" -computername $VM | where {$_.SCSIBus -eq $VirtualSCSIController.BusNumber -and $_.SCSITargetID -eq $VirtualDiskDevice.UnitNumber}
+                    $thisVirtualDisk = get-wmiobject -class "Win32_DiskDrive" -namespace "root\CIMV2" -computername $VM -Credential $ADcreds | where {$_.SCSIBus -eq $VirtualSCSIController.BusNumber -and $_.SCSITargetID -eq $VirtualDiskDevice.UnitNumber}
                     # Look up partition using WMI.
-                    $Disk2Part = Get-WmiObject Win32_DiskDriveToDiskPartition -computername $VM | Where {$_.Antecedent -eq $thisVirtualDisk.__Path}
+                    $Disk2Part = Get-WmiObject Win32_DiskDriveToDiskPartition -computername $VM -Credential $ADcreds | Where {$_.Antecedent -eq $thisVirtualDisk.__Path}
                     foreach ($thisPartition in $Disk2Part) {
                         #Look up logical drives for that partition using WMI.
-                        $Part2Log = Get-WmiObject -Class Win32_LogicalDiskToPartition -computername $VM | Where {$_.Antecedent -eq $thisPartition.Dependent}
+                        $Part2Log = Get-WmiObject -Class Win32_LogicalDiskToPartition -computername $VM -Credential $ADcreds | Where {$_.Antecedent -eq $thisPartition.Dependent}
                         foreach ($thisLogical in $Part2Log) {
                             if ($thisLogical.Dependent -match "[A-Z]:") {
                                 $LogicalDisks += $matches[0]
@@ -36,7 +34,5 @@ Function Get-VMDiskMap {
                 }
             }
         }
-    }
-    end {
     }
 }
